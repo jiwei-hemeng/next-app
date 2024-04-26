2479174cb7fd28a71dce8d1a7c4b94e3c541b299
jijiwei <jijiwei@lilianinfo.com>
2024-04-26 10:42:34 +0800

    init

---
+++ src/app/about/layout.js
@@ -0,0 +1,9 @@
+import "./globals.css";
+export const metadata = {
+  title: "ABOUT | NEXT REALWORLD",
+  description:
+    "Please login to use fully-featured next-realworld site. (Post articles, comments, and like, follow etc.)",
+};
+export default function RootLayout({ children }) {
+  return children;
+}
